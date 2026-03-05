1ec13cd4c857a4ce263828f0f619d5080d4ea40b
Arron <arron.roasa@gmail.com>
2026-03-04 17:51:31 -0700

    Implemented delete city test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,5 +1,6 @@
 package com.example.lab08;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -14,4 +15,12 @@ public class CustomListTest {
         assertTrue(list.hasCity(calgary));
     }
 
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City vancouver = new City("Vancouver", "BC");
+        list.addCity(vancouver);
+
+        assertFalse(list.hasCity(vancouver));
+    }
 }
